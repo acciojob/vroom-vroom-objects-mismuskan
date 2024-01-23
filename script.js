43937499521ca28c34597bf4f07268c2dfa9393c
Muskan Sheikh <144351188+mismuskan@users.noreply.github.com>
2024-01-23 22:06:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make=make;
+	this.model=model;
+}
+Car.prototype.getMakeModel=function(){
+	  return `${this.make} ${this.model}`;
+};
+function SportsCar(make, model, topSpeed) {
+	 Car.call(this, make, model);
+	 this.topSpeed = topSpeed;
+	
+}
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.getTopSpeed = function () {
+  return this.topSpeed;
+};
+const regularCar = new Car("Toyota", "Camry");
+console.log(regularCar.getMakeModel()); // Output: Toyota Camry
 
-function SportsCar(make, model, topSpeed) {}
-
+const sportsCar = new SportsCar("Ferrari", "488 GTB", 205);
+console.log(sportsCar.getMakeModel()); // Output: Ferrari 488 GTB
+console.log(sportsCar.getTopSpeed());
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
